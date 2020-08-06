7a74b4b8e39735a03115ff5089d803beeaac02fc
Alexander Rice <webdev@ricealexander.com>
2020-08-06 15:02:14 -0500

    :sparkles: Create form toggler
    Toggle between black version and white version of logo

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ import { embedLogo, setGradient } from './scripts'
 // Let's begin
 embedLogo('.logo-container', 'pulse')
 setGradient(['red', 'orange', 'yellow'])
+
+// Add Event Listeners to Controls
+
+// Logo Forms
+const buttonFormBlack = document.querySelector('#form-black')
+const buttonFormWhite = document.querySelector('#form-white')
+const preview = document.querySelector('.logo-preview')
+
+buttonFormBlack.addEventListener('click', () => {
+  preview.classList.remove('inverse')
+  buttonFormWhite.classList.remove('active')
+  buttonFormBlack.classList.add('active')
+})
+
+buttonFormWhite.addEventListener('click', () => {
+  preview.classList.add('inverse')
+  buttonFormBlack.classList.remove('active')
+  buttonFormWhite.classList.add('active')
+})
